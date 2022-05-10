dadd171dfd7c78f8be6873727fbf122ff18b0753
varshaprasad96 <varshaprasad96@gmail.com>
2022-05-10 12:15:30 -0700

    Add godoc to the templates

--- testdata/pkg/clusterclient/typed/example/v1/examplev1.go
+++ testdata/pkg/clusterclient/typed/example/v1/examplev1.go
@@ -35,19 +35,24 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+// WrappedExampleV1 wraps the client interface with a
+// logical cluster.
 type WrappedExampleV1 struct {
 	cluster  logicalcluster.LogicalCluster
 	delegate examplev1.ExampleV1Interface
 }
 
+// New creates a WrappedExampleV1 with the given logical cluster and client interface.
 func New(cluster logicalcluster.LogicalCluster, delegate examplev1.ExampleV1Interface) *WrappedExampleV1 {
 	return &WrappedExampleV1{cluster: cluster, delegate: delegate}
 }
 
+// RESTClient returns the underlying RESTClient.
 func (w *WrappedExampleV1) RESTClient() rest.Interface {
 	return w.delegate.RESTClient()
 }
 
+// WrappedExampleV1 contains the wrapped logical cluster and interface.
 func (w *WrappedExampleV1) ClusterTestTypes() examplev1.ClusterTestTypeInterface {
 	return &wrappedClusterTestType{
 		cluster:  w.cluster,
@@ -60,6 +65,9 @@ type wrappedClusterTestType struct {
 	delegate examplev1.ClusterTestTypeInterface
 }
 
+// checkCluster retrives the logical cluster name from the given context and checks
+// if it is the same as the one passed while creating a wrappedClusterTestType. It errors when
+// there is a mismatch.
 func (w *wrappedClusterTestType) checkCluster(ctx context.Context) (context.Context, error) {
 	ctxCluster, ok := kcp.ClusterFromContext(ctx)
 	if !ok {
@@ -70,6 +78,7 @@ func (w *wrappedClusterTestType) checkCluster(ctx context.Context) (context.Cont
 	return ctx, nil
 }
 
+// Create implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Create(ctx context.Context, clusterTestType *exampleapiv1.ClusterTestType, opts metav1.CreateOptions) (*exampleapiv1.ClusterTestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -78,6 +87,7 @@ func (w *wrappedClusterTestType) Create(ctx context.Context, clusterTestType *ex
 	return w.delegate.Create(ctx, clusterTestType, opts)
 }
 
+// Update implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Update(ctx context.Context, clusterTestType *exampleapiv1.ClusterTestType, opts metav1.UpdateOptions) (*exampleapiv1.ClusterTestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -86,6 +96,7 @@ func (w *wrappedClusterTestType) Update(ctx context.Context, clusterTestType *ex
 	return w.delegate.Update(ctx, clusterTestType, opts)
 }
 
+// UpdateStatus implements ClusterTestTypeInterface. It was generated because the type contains a Status member.
 func (w *wrappedClusterTestType) UpdateStatus(ctx context.Context, clusterTestType *exampleapiv1.ClusterTestType, opts metav1.UpdateOptions) (*exampleapiv1.ClusterTestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -94,6 +105,7 @@ func (w *wrappedClusterTestType) UpdateStatus(ctx context.Context, clusterTestTy
 	return w.delegate.UpdateStatus(ctx, clusterTestType, opts)
 }
 
+// Update implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -102,6 +114,7 @@ func (w *wrappedClusterTestType) Delete(ctx context.Context, name string, opts m
 	return w.delegate.Delete(ctx, name, opts)
 }
 
+// DeleteCollection implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listopts metav1.ListOptions) error {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -110,6 +123,7 @@ func (w *wrappedClusterTestType) DeleteCollection(ctx context.Context, opts meta
 	return w.delegate.DeleteCollection(ctx, opts, listopts)
 }
 
+// Get implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Get(ctx context.Context, name string, opts metav1.GetOptions) (*exampleapiv1.ClusterTestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -118,6 +132,7 @@ func (w *wrappedClusterTestType) Get(ctx context.Context, name string, opts meta
 	return w.delegate.Get(ctx, name, opts)
 }
 
+// List implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) List(ctx context.Context, opts metav1.ListOptions) (*exampleapiv1.ClusterTestTypeList, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -126,6 +141,7 @@ func (w *wrappedClusterTestType) List(ctx context.Context, opts metav1.ListOptio
 	return w.delegate.List(ctx, opts)
 }
 
+// Watch implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -134,6 +150,7 @@ func (w *wrappedClusterTestType) Watch(ctx context.Context, opts metav1.ListOpti
 	return w.delegate.Watch(ctx, opts)
 }
 
+// Patch implements ClusterTestTypeInterface.
 func (w *wrappedClusterTestType) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *exampleapiv1.ClusterTestType, err error) {
 	ctx, err = w.checkCluster(ctx)
 	if err != nil {
@@ -142,6 +159,7 @@ func (w *wrappedClusterTestType) Patch(ctx context.Context, name string, pt type
 	return w.delegate.Patch(ctx, name, pt, data, opts, subresources...)
 }
 
+// WrappedExampleV1 contains the wrapped logical cluster and interface.
 func (w *WrappedExampleV1) TestTypes(namespace string) examplev1.TestTypeInterface {
 	return &wrappedTestType{
 		cluster:  w.cluster,
@@ -154,6 +172,9 @@ type wrappedTestType struct {
 	delegate examplev1.TestTypeInterface
 }
 
+// checkCluster retrives the logical cluster name from the given context and checks
+// if it is the same as the one passed while creating a wrappedTestType. It errors when
+// there is a mismatch.
 func (w *wrappedTestType) checkCluster(ctx context.Context) (context.Context, error) {
 	ctxCluster, ok := kcp.ClusterFromContext(ctx)
 	if !ok {
@@ -164,6 +185,7 @@ func (w *wrappedTestType) checkCluster(ctx context.Context) (context.Context, er
 	return ctx, nil
 }
 
+// Create implements TestTypeInterface.
 func (w *wrappedTestType) Create(ctx context.Context, testType *exampleapiv1.TestType, opts metav1.CreateOptions) (*exampleapiv1.TestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -172,6 +194,7 @@ func (w *wrappedTestType) Create(ctx context.Context, testType *exampleapiv1.Tes
 	return w.delegate.Create(ctx, testType, opts)
 }
 
+// Update implements TestTypeInterface.
 func (w *wrappedTestType) Update(ctx context.Context, testType *exampleapiv1.TestType, opts metav1.UpdateOptions) (*exampleapiv1.TestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -180,6 +203,7 @@ func (w *wrappedTestType) Update(ctx context.Context, testType *exampleapiv1.Tes
 	return w.delegate.Update(ctx, testType, opts)
 }
 
+// Update implements TestTypeInterface.
 func (w *wrappedTestType) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -188,6 +212,7 @@ func (w *wrappedTestType) Delete(ctx context.Context, name string, opts metav1.D
 	return w.delegate.Delete(ctx, name, opts)
 }
 
+// DeleteCollection implements TestTypeInterface.
 func (w *wrappedTestType) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listopts metav1.ListOptions) error {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -196,6 +221,7 @@ func (w *wrappedTestType) DeleteCollection(ctx context.Context, opts metav1.Dele
 	return w.delegate.DeleteCollection(ctx, opts, listopts)
 }
 
+// Get implements TestTypeInterface.
 func (w *wrappedTestType) Get(ctx context.Context, name string, opts metav1.GetOptions) (*exampleapiv1.TestType, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -204,6 +230,7 @@ func (w *wrappedTestType) Get(ctx context.Context, name string, opts metav1.GetO
 	return w.delegate.Get(ctx, name, opts)
 }
 
+// List implements TestTypeInterface.
 func (w *wrappedTestType) List(ctx context.Context, opts metav1.ListOptions) (*exampleapiv1.TestTypeList, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -212,6 +239,7 @@ func (w *wrappedTestType) List(ctx context.Context, opts metav1.ListOptions) (*e
 	return w.delegate.List(ctx, opts)
 }
 
+// Watch implements TestTypeInterface.
 func (w *wrappedTestType) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
 	ctx, err := w.checkCluster(ctx)
 	if err != nil {
@@ -220,6 +248,7 @@ func (w *wrappedTestType) Watch(ctx context.Context, opts metav1.ListOptions) (w
 	return w.delegate.Watch(ctx, opts)
 }
 
+// Patch implements TestTypeInterface.
 func (w *wrappedTestType) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *exampleapiv1.TestType, err error) {
 	ctx, err = w.checkCluster(ctx)
 	if err != nil {
