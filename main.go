85982e70a18f40d0eea90ec1a032023c57261564
Andy Goldstein <andy.goldstein@redhat.com>
2022-05-11 11:09:53 -0400

    main.go: fix boilerplate
    Signed-off-by: Andy Goldstein <andy.goldstein@redhat.com>

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package main
 
 import (
@@ -20,12 +21,13 @@ import (
 	"os"
 	"strings"
 
-	"github.com/kcp-dev/code-generator/pkg/flag"
-	"github.com/kcp-dev/code-generator/pkg/generators"
-	"github.com/kcp-dev/code-generator/pkg/generators/clientgen"
 	"github.com/spf13/cobra"
 	"sigs.k8s.io/controller-tools/pkg/genall"
 	"sigs.k8s.io/controller-tools/pkg/markers"
+
+	"github.com/kcp-dev/code-generator/pkg/flag"
+	"github.com/kcp-dev/code-generator/pkg/generators"
+	"github.com/kcp-dev/code-generator/pkg/generators/clientgen"
 )
 
 var (
